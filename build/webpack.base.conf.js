9c510bbbf0c90d08a06f6960626a40f23199cca5
liuy <quickn@qq.com>
2019-08-06 21:33:49 +0800

    mpvue升级

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -61,9 +61,7 @@ module.exports = {
           'babel-loader',
           {
             loader: 'mpvue-loader',
-            options: {
-              checkMPEntry: true
-            }
+            options: Object.assign({checkMPEntry: true}, vueLoaderConfig)
           },
         ]
       },
